a77a65611cd0bae2761e39c9c90b5fa927f6d62c
Patricia Garcia <patijoe33@gmail.com>
2019-06-28 09:30:08 +0200

    Add images

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
 import React from 'react';
+import diachungo from './images/diachungo.jpeg';
+import patri from './images/patri.jpg';
 import Header from './components/Header';
 import Conoceme from './components/Conoceme';
 import Aptitudes from './components/Aptitudes';
 import Queseyo from './components/Queseyo';
 
 class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      imagen: (patri)
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange() {
+    this.setState({
+      imagen: (diachungo)
+    })
+
+    console.log('^^', this.state.imagen);
+  }
+
   render() {
+  const {imagen} =this.state;
+
     return (
       <div className="app">
         <Header />
 
         <main className="main">
           
-          <Conoceme />
+          <Conoceme 
+            imagen={imagen}
+            handleChange={this.handleChange} 
+          />
           <Aptitudes />
           <Queseyo />
         </main>
--- src/components/Conoceme.js
+++ src/components/Conoceme.js
@@ -1,37 +1,52 @@
 import React from 'react';
-import patri from '../images/patri.jpg';
-import diachungo from '../images/diachungo.jpeg';
 
 class Conoceme extends React.Component {
   render() {
+    const {imagen, handleChange} = this.props;
 
     return (
       <section className="conoceme" id="conoceme">
+
         <div className="conoceme__info">
           <h1 className="conoceme__name">Patricia García</h1>
           <h2 className="conoceme__job">Desarrolladora front-end</h2>
         </div>
-        <div className="conoceme__img" style={{backgroundImage: `url(${patri})`}}></div>
+
+        <div className="conoceme__block1">
+          <img src={imagen} alt="" className="conoceme__img"/>
+          <p className="conoceme__text">
+            Tras varios años en una start-up de desarrollo software ocupándome de todo tipo de funciones que nada tenían que ver con el código, me descubrí mirando con curiosidad y envidia (de la sana) ese extraño campo.
+            <br /><br />
+            Decidí cruzar la línea y toparme con la programación 🚀. He descubierto mi aptitud en este campo y he pivotado mi carrera profesional para reconvertirme en desarrolladora Front-end con la ayuda de Adalab.
+            <br /><br />
+            Mi formación como Ingeniera complementa este perfil al que hay sumarle mis ganas inagotables de seguir avanzando y de enfrentarme a nuevos retos 🤖.
+            <br /><br />
+          </p>
+        </div>
+
         <p className="conoceme__text">
-        Tras varios años en una start-up de desarrollo software ocupándome de todo tipo de funciones que nada tenían que ver con el código, me descubrí mirando con curiosidad y envidia (de la sana) ese extraño campo.
+        {/* Tras varios años en una start-up de desarrollo software ocupándome de todo tipo de funciones que nada tenían que ver con el código, me descubrí mirando con curiosidad y envidia (de la sana) ese extraño campo.
 
-        <br /><br />
+        <br /><br /> */}
 
-        Decidí cruzar la línea y toparme con la programación 🚀. He descubierto mi aptitud en este campo y he pivotado mi carrera profesional para reconvertirme en desarrolladora Front-end con la ayuda de Adalab.
+        {/* Decidí cruzar la línea y toparme con la programación 🚀. He descubierto mi aptitud en este campo y he pivotado mi carrera profesional para reconvertirme en desarrolladora Front-end con la ayuda de Adalab.
 
-        <br /><br />
+        <br /><br /> */}
 
-        Mi formación como Ingeniera complementa este perfil al que hay sumarle mis ganas inagotables de seguir avanzando y de enfrentarme a nuevos retos 🤖.
+        {/* Mi formación como Ingeniera complementa este perfil al que hay sumarle mis ganas inagotables de seguir avanzando y de enfrentarme a nuevos retos 🤖.
 
-        <br /><br />
+        <br /><br /> */}
 
         Comprometida, analítica y proactiva, acostumbrada a trabajar en equipos multifuncionales.
 
         <br /><br />
 
-        Vale, vale, todo está genial, pero cuando llega un día chungo... de los de verdad... 
-        <a href={diachungo} className="dia__chungo">¿quieres saber que pasa?</a>
+        Vale, vale, todo está genial, pero cuando llega un día chungo... de los de verdad... si no quieres saber que pasa, no pulses el botón rojo bajo ningún concepto.
         </p>
+
+        <div className="conoceme__red-button" onClick={handleChange}>
+          NO PULSAR <i className="fas fa-skull"></i>
+        </div>
       </section>
     );
   }
